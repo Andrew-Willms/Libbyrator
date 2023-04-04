0bd0c2cdf3cce5ca2f7a0bdad07b98223774eca4
Andrew-Willms <andrew.e.willms@gmail.com>
2023-04-04 00:03:14 -0400

    Formatting and adding nuget package. Started fleshingout UI.

--- Libbyrator/Libbyrator/MainPage.xaml.cs
+++ Libbyrator/Libbyrator/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
 
 namespace Libbyrator;
 
 
 
 public partial class MainPage : ContentPage {
+
+	public string TargetDirectoryPath { get; set; } = "";
+
+	public ObservableCollection<string> Test { get; set; } = new();
 
 	public MainPage() {
 		InitializeComponent();
